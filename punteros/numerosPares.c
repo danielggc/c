1a6a3dc19f333b0d33fdf049ec4979b8499472f9
Your Name <josemase55@gmail.com>
2018-12-31 09:01:14 -0500

    atualixacion

--- punteros/numerosPares.c
+++ punteros/numerosPares.c
@@ -8,12 +8,12 @@ int main(){
   scanf("%d  ",&dato );
   if  (dato%2==0){
     printf("el valor %d",dato);
-    printd(" es par ,y su direccion de memoria es"&dato )
+    printf(" es par ,y su direccion de memoria es  %d",&dato );
    }
  
    if  (dato%2!=0){
      printf("el valor %d",dato);
-     printd(" es inpar ,y su direccion de memoria es"&dato )
+     printf(" es inpar ,y su direccion de memoria es %d",&dato );
    }
   
 
